930f86a6f2f0d6e3f0f785be48d0a7b9d91c2b75
Barca545 <124243414+Barca545@users.noreply.github.com>
2025-03-02 02:11:45 -0500

    - Added a LOOP register and updated the FIRST_FREE_REGISTER value.

--- src/registers.rs
+++ src/registers.rs
@@ -1,8 +1,8 @@
 /// Number of registers in the [`VM`](https://github.com/Barca545/galaxy).
 pub const REG_COUNT:usize = u8::MAX as usize;
 
-/// First non-reserved register in the [`VM`](https://github.com/Barca545/galaxy). Registers R3-R12 are reserved for function arguments.
-pub const FIRST_FREE_REGISTER:usize = 12;
+/// First non-reserved register in the [`VM`](https://github.com/Barca545/galaxy). Registers R4-R13 are reserved for function arguments.
+pub const FIRST_FREE_REGISTER:usize = 13;
 
 /// Program counter. Contains the address of the next
 /// [`OpCode`](crate::opcodes::OpCode) instruction.
@@ -13,3 +13,6 @@ pub const SP:usize = 1;
 
 /// Register which holds the result of the [`VM`](https://github.com/Barca545/galaxy)'s last equality check.
 pub const EQ:usize = 2;
+
+/// Register which holds the [loop variable](https://en.wikipedia.org/wiki/For_loop) of the [`VM`](https://github.com/Barca545/galaxy)'s currently executing loop
+pub const LOOP:usize = 3;
